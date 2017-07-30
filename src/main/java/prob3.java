0ece96ce26ca1ca74696212fa82f23f955dc19af
Vevin Moza <vevin_moza@s5a.com>
2017-07-29 20:23:17 -0400

    solution to problem 3

---
+++ src/main/java/prob3.java
@@ -0,0 +1,24 @@
+/**
+ * Created by vevinmoza on 7/29/17.
+ */
+public class prob3 {
+    public static void main(String[] args){
+    Long number=new Long("600851475143");
+
+    int index=2;
+    int maxPrime=2;
+    while(number!=1){
+        if(number%index==0){
+            number=number/index;
+            if(maxPrime<index){
+                maxPrime=index;
+            }
+        }else{
+            index++;
+        }
+    }
+    System.out.println(maxPrime);
+
+
+    }
+}
