ad1146e78d07d65203d6501e4fa716963dcd1d7a
Vevin Moza <vevin.moza@gmail.com>
2017-08-01 00:13:36 -0400

    MinimumRotationsSameString

--- src/main/java/com/geeksforgeeks/solutions/MinimumRotationsSameString.java
+++ src/main/java/com/geeksforgeeks/solutions/MinimumRotationsSameString.java
@@ -2,10 +2,17 @@ package com.geeksforgeeks.solutions;
 
 public class MinimumRotationsSameString {
     public static void main(String[] args){
-        String s = "geeks";
+        String s = "2332242142142142";
         String s1=s;
-        char[] charArr=s.toCharArray();
+        int i=1;
+        while(i<=s.length()){
+            String rt=s.substring(i)+s.substring(0,i);
+            if(rt.equals(s)){
+                break;
+            }
 
-
+            i++;
+        }
+        System.out.println(i);
     }
 }
